702842fc0d6a554188948180a0c599a6b4998e4d
Wang Shenyu <912059677@qq.com>
2019-03-21 19:22:36 +0800

    修改了get_all_payday中的错误，该错误会导致得到的还款日期失去最后一期

--- time_sy.py
+++ time_sy.py
@@ -14,11 +14,9 @@ def compare_time(time1, time2):
 
 
 def get_all_payday(first, stage):  # 输入第一个还款日与期数，输出一个由所有还款日期组成的数组
-    i = 2
     all_payday = [first]
     first_time = time.strptime(first, '%Y%m%d')
-    for i in range(2, stage):
-        i = i + 1
+    for i in range(1, stage):
         next_time_tm_mon = first_time.tm_mon + 1
         next_time_tm_year = first_time.tm_year
         if next_time_tm_mon == 13:
@@ -36,8 +34,8 @@ def getpastday(time1, time2):
     :param time2:
     :return: 返回两个以"YYYYMMDD"为格式的日期之间的天数差，返回值的类型为数值
     """
-    time1 = time.strptime("19980817","%Y%m%d")
-    time2 = time.strptime("19980818","%Y%m%d")
+    time1 = time.strptime(time1,"%Y%m%d")
+    time2 = time.strptime(time2,"%Y%m%d")
     time3 = abs(time.mktime(time2) - time.mktime(time1))
     day = time3/(60*60*24)
     return day
